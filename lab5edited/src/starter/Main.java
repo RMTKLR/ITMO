2aa90c70230216097ed778b1cdd19e54be188a5c
RMTKLR <119819466+RMTKLR@users.noreply.github.com>
2024-02-22 16:56:59 +0300

    Main.java

--- lab5edited/src/starter/Main.java
+++ lab5edited/src/starter/Main.java
@@ -9,27 +9,29 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-
-        FileManager fileManager = new FileManager(args[0]);
-
-        CollectionManager collectionManager = new CollectionManager();
-
-        CollectionDeserializer collectionDeserializer = new CollectionDeserializer(collectionManager);
-
-        String fileContent = fileManager.reader();
-        if (!fileContent.equals("")) {
-            if(collectionDeserializer.deserialize(fileManager.reader()))
-                System.out.println("Data has been successfully loaded.");
+        try{
+            FileManager fileManager = new FileManager(args[0]);
+    
+            CollectionManager collectionManager = new CollectionManager();
+    
+            CollectionDeserializer collectionDeserializer = new CollectionDeserializer(collectionManager);
+    
+            String fileContent = fileManager.reader();
+            if (!fileContent.equals("")) {
+                if(collectionDeserializer.deserialize(fileManager.reader()))
+                    System.out.println("Data has been successfully loaded.");
+            }
+            else if(fileContent.equals(""))
+                System.out.println("collection is empty!!");
+            else System.err.println("can't load the data!!!");
+    
+            CommandManager commandManager = new CommandManager(fileManager,collectionManager);
+    
+            Runner run = new Runner(commandManager.getCommandMap());
+            run.runner();
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.err.println("Wrong or NO address passes to the argument!!!");
         }
-        else if(fileContent.equals(""))
-            System.out.println("collection is empty!!");
-        else System.err.println("can't load the data!!!");
-
-        CommandManager commandManager = new CommandManager(fileManager,collectionManager);
-
-        Runner run = new Runner(commandManager.getCommandMap());
-        run.runner();
-
 
 
     }
